10994e1c27e17e2e05bf768f9e1b87a3b1aa253c
Logan Henrie <loghenrie@gmail.com>
2023-05-11 00:37:45 -0400

    Revert "Feature/image uploading"

--- backend/db/ingredientModel.js
+++
@@ -1,8 +0,0 @@
-export default function (mongoose) {
-	const schema = new mongoose.Schema({
-		name: String,
-		recipes: [{ type: mongoose.ObjectId, ref: "Recipe" }],
-	});
-	const Model = mongoose.model("Ingredient", schema);
-	return Model;
-}
