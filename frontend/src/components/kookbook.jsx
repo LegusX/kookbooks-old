c7a693fe0b50092fe2cad1ca217abf889dffe2ef
Logan Henrie <loghenrie@gmail.com>
2023-05-13 22:28:58 -0400

    add transition on book hover

--- frontend/src/components/kookbook.jsx
+++ frontend/src/components/kookbook.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Kookbook({ book }) {
 	return (
 		<Link to={"/books/" + book.id}>
-			<div className="card card-compact w-full drop-shadow-md rounded-xl cursor-pointer">
+			<div className="card card-compact w-full drop-shadow-md rounded-xl cursor-pointer hover:scale-105 transition:all ease-in-out duration-100">
 				<figure className=" bg-primary min-h-8"></figure>
 				<div className="card-body bg-base-100 rounded-b-md flex content-between">
 					<h2 className="card-title">{book.name}</h2>
