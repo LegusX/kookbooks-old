302614e7a9f4a824e846462129017785be21f63c
Logan Henrie <loghenrie@gmail.com>
2023-05-15 01:45:27 -0400

    add initial signup page layout

---
+++ frontend/src/routes/signup.jsx
@@ -0,0 +1,104 @@
+import { useContext, useRef } from "react";
+import { UserContext } from "../App";
+import { useNavigate } from "react-router-dom";
+
+export default function SignupRoute() {
+	const { user } = useContext(UserContext);
+	const navigate = useNavigate();
+
+	const name = useRef();
+	const username = useRef();
+	const email = useRef();
+	const password = useRef();
+	const confirmPassword = useRef();
+
+	//TODO: Validation
+	const submit = () => {};
+
+	if (user !== null && user.id) navigate("/home");
+
+	if (user === null) {
+		return (
+			<div className="hero flex flex-col algin-middle bg-base-100 lg:bg-base-200 min-h-full">
+				<div className="card lg:shadow-2xl bg-base-100 lg:mt-28 card-normal">
+					<div className="card-body">
+						<h2 className="card-title justify-center">Sign up</h2>
+						<div className="flex flex-wrap gap-4 lg:gap-8 justify-center">
+							<div>
+								<div className="form-control">
+									<label className="label">
+										<span className="label-text">Your name</span>
+									</label>
+									<input
+										className="input input-bordered"
+										type="text"
+										placeholder="John Doe"
+										ref={name}
+										maxLength="30"
+									></input>
+								</div>
+								<div className="form-control">
+									<label className="label">
+										<span className="label-text">Username</span>
+									</label>
+									<input
+										className="input input-bordered"
+										type="text"
+										placeholder="john.doe"
+										ref={username}
+										maxLength="20"
+									></input>
+								</div>
+								<div className="form-control">
+									<label className="label">
+										<span className="label-text">Email</span>
+									</label>
+									<input
+										className="input input-bordered"
+										type="text"
+										placeholder="john.doe@site.com"
+										ref={email}
+										maxLength="100"
+									></input>
+								</div>
+							</div>
+							<div className="divider lg:hidden"></div>
+							<div>
+								<div className="form-control">
+									<label className="label">
+										<span className="label-text">Password</span>
+									</label>
+									<input
+										className="input input-bordered"
+										type="text"
+										placeholder="********"
+										ref={password}
+										maxLength="32"
+									></input>
+								</div>
+								<div className="form-control">
+									<label className="label">
+										<span className="label-text">Confirm Password</span>
+									</label>
+									<input
+										className="input input-bordered"
+										type="text"
+										placeholder="********"
+										ref={confirmPassword}
+										maxLength="32"
+									></input>
+								</div>
+							</div>
+						</div>
+						<button
+							className="btn btn-primary btn-outline mt-4"
+							onClick={() => submit()}
+						>
+							Sign up
+						</button>
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui";
 
-export default {
+module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {},
 	},
-	plugins: [daisyui],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 	daisyui: {
 		themes: ["cupcake"],
 	},
