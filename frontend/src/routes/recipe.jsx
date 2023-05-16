e36c251a9560f4d70d225e9cbfc944dcad0be30e
Logan Henrie <loghenrie@gmail.com>
2023-05-16 18:18:08 -0400

    add recipe getter from backend

--- frontend/src/routes/recipe.jsx
+++ frontend/src/routes/recipe.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { getRecipe } from "../api/recipe";
 
 export default function RecipeRoute() {
 	const { recipeID } = useParams();
+	const [recipe, setRecipe] = useState(null);
 
-	return <p>{recipeID}</p>;
+	useEffect(() => {
+		getRecipe(recipeID).then((data) => {
+			if (data === null) return; //TODO: Handle 404 better
+			setRecipe(data);
+		});
+	}, []);
+
+	if (recipe === null)
+		return (
+			<div className="flex min-w-full min-h-full justify-center items-center mt-10">
+				<div
+					className="radial-progress animate-spin"
+					style={{ "--value": 70 }}
+				/>
+			</div>
+		);
+
+	return <p>{recipe.id}</p>;
 }
